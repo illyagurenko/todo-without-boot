4a8a41d26e76784904dcc943142f41bc06fbb335
IlyaGurenko <illya4gurenko@gmail.com>
2026-05-18 22:14:40 +0300

    simplify dao with @Transactional

--- src/main/java/ru/example/todo_without_boot/dao/TaskDao.java
+++ src/main/java/ru/example/todo_without_boot/dao/TaskDao.java
@@ -2,7 +2,9 @@ package ru.example.todo_without_boot.dao;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.example.todo_without_boot.entity.TaskStatus;
@@ -12,82 +14,35 @@ import java.util.*;
 
 @Repository
 public class TaskDao {
-    private final EntityManagerFactory entityManagerFactory;
+    @PersistenceContext
+    private EntityManager entityManager;
 
-    @Autowired
-    public TaskDao(EntityManagerFactory entityManagerFactory) {
-        this.entityManagerFactory = entityManagerFactory;
-    }
-
+    @Transactional
     public List<Task> findAllTasks() {
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        try{
-            entityManager.getTransaction().begin();
-
-            Query query = entityManager.createQuery("select t from Task t");
-            List<Task> tasks = query.getResultList();
-
-            entityManager.getTransaction().commit();
-
-            return tasks;
-        } catch (Exception e) {
-            e.printStackTrace();
-            entityManager.getTransaction().rollback();
-            return Collections.emptyList();
-        }
-        finally {
-            entityManager.close();
-        }
+        Query query = entityManager.createQuery("select t from Task t");
+        List<Task> tasks = query.getResultList();
+        return tasks;
     }
 
-    public void saveTask(Task task){
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        try{
-            entityManager.getTransaction().begin();
-            entityManager.persist(task);
-            entityManager.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-            entityManager.getTransaction().rollback();
-        }
-        finally {
-            entityManager.close();
-        }
+    @Transactional
+    public void saveTask(Task task) {
+        entityManager.persist(task);
     }
 
-    public void updateTaskStatus(int id, TaskStatus status){
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        try{
-            entityManager.getTransaction().begin();
-            Query query = entityManager.createQuery("update Task set status = :status where id = :id");
-            query.setParameter("id", id);
-            query.setParameter("status", status);
-            query.executeUpdate();
-            entityManager.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-            entityManager.getTransaction().rollback();
-        }
-        finally {
-            entityManager.close();
-        }
+    @Transactional
+    public void updateTaskStatus(int id, TaskStatus status) {
+        Query query = entityManager.createQuery("update Task set status = :status where id = :id");
+        query.setParameter("id", id);
+        query.setParameter("status", status);
+        query.executeUpdate();
     }
 
-    public void deleteTask(int id){
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        try{
-            entityManager.getTransaction().begin();
-            Query query = entityManager.createQuery("delete from Task where id = :id");
-            query.setParameter("id", id);
-            query.executeUpdate();
-            entityManager.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-            entityManager.getTransaction().rollback();
-        }
-        finally {
-            entityManager.close();
-        }
+    @Transactional
+    public void deleteTask(int id) {
+        Query query = entityManager.createQuery("delete from Task where id = :id");
+        query.setParameter("id", id);
+        query.executeUpdate();
+        entityManager.getTransaction().commit();
     }
 
 }
